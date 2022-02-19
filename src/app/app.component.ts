646c79a71974c11f1ddbe38195bfb8dc75ca82fa
Daniela Meneses <dvargas@oildex.com>
2022-02-18 20:08:15 -0400

    Initializa angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tsday-eslint-angular-example';
+}
